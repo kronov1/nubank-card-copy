13ed9890d92a3fe1188b2a424ed3c748b0d053a7
bruno <brunooliveira22br74@gmail.com>
2024-04-10 03:16:27 -0300

    com

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeText() {
+
+    var inputField = document.getElementById("input-text");
+    var displayText = document.getElementById("display-text");
+
+    displayText.textContent = inputField.value
+
+}
+
